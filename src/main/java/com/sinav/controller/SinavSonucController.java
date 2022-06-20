59e83068d79c5c299b0c9d4bf31b330651abc50f
Savaş Dede <svsdd22@gmail.com>
2022-06-20 22:30:25 +0300

    keyloack development

--- src/main/java/com/sinav/controller/SinavSonucController.java
+++ src/main/java/com/sinav/controller/SinavSonucController.java
@@ -2,6 +2,7 @@ package com.sinav.controller;
 
 import com.sinav.dto.SinavSonucDto;
 import com.sinav.service.SinavSonucService;
+import com.sinav.utils.SinavUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -20,30 +22,35 @@ public class SinavSonucController {
 
     @Validated
     @GetMapping("/sinavsonucs")
+    @RolesAllowed({SinavUtil.KEYLOACK_ROL})
     public ResponseEntity<List<SinavSonucDto>> getAllSonuc() {
         return new ResponseEntity<>(service.getSonuc().getAllSonuc(), HttpStatus.OK);
     }
 
     @Validated
     @GetMapping("/sinavs/sinavsonucs/{id}")
+    @RolesAllowed({SinavUtil.KEYLOACK_ROL})
     public ResponseEntity<List<SinavSonucDto>> getSonucBySinav(@PathVariable String id) {
         return new ResponseEntity<>(service.getSonuc().getSonucBySinav(id), HttpStatus.OK);
     }
 
     @Transactional
     @PostMapping("/sinavsonucs")
+    @RolesAllowed({SinavUtil.KEYLOACK_ROL})
     public ResponseEntity<SinavSonucDto> postSonuc(@RequestBody SinavSonucDto dto) {
         return new ResponseEntity<>(service.getSonuc().createSonuc(dto), HttpStatus.CREATED);
     }
 
     @Transactional
     @PutMapping("/sinavsonucs/{id}")
+    @RolesAllowed({SinavUtil.KEYLOACK_ROL})
     public ResponseEntity<SinavSonucDto> putSonuc(@PathVariable String id, @RequestBody SinavSonucDto dto) {
         return new ResponseEntity<>(service.getSonuc().updateSonuc(id, dto), HttpStatus.OK);
     }
 
     @Transactional
     @DeleteMapping("/sinavsonucs/{id}")
+    @RolesAllowed({SinavUtil.KEYLOACK_ROL})
     public ResponseEntity<?> deleteSonuc(@PathVariable String id) {
         service.getSonuc().deleteSonuc(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
