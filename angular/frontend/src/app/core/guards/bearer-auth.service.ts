b8719cc4d5e86d5b5623aec2a901cf22968ae318
Savaş Dede <svsdd22@gmail.com>
2022-07-16 21:55:04 +0300

    add guards to auth-service

---
+++ angular/frontend/src/app/core/guards/bearer-auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { SessionStorageService } from 'angular-web-storage';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BasicAuthService implements CanActivate {
+    constructor(
+        private router: Router,
+        private sessionStorage: SessionStorageService
+    ) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const currentUser = this.sessionStorage.get('UserModel');
+        if (currentUser) {
+            return true;
+        } else {
+            this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+            return false;
+        }
+    }
+}
